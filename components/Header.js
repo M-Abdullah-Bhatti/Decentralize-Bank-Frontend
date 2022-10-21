a9d1115749aa1ea6f597e4cbac2fba8753a7acbf
Raza <mraza6601@gmail.com>
2022-10-21 22:34:42 +0500

    deposit funds implemented

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,20 @@
-import React from "react";
 
-const Header = () => {
-  return <div>Header</div>;
-};
+import { ConnectButton } from "web3uikit"
+import Link from "next/link"
 
-export default Header;
+export default function Header() {
+    return (
+        <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
+            <h1 className="py-4 px-4 font-bold text-3xl"> Dbank</h1>
+            <div className="flex flex-row items-center">
+                <Link href="/">
+                    <a className="mr-4 p-6">Home</a>
+                </Link>
+                <Link href="/Deposit">
+                    <a className="mr-4 p-6">Deposit</a>
+                </Link>
+                <ConnectButton moralisAuth={false} />
+            </div>
+        </nav>
+    )
+}
--- pages/Deposit.js
+++ pages/Deposit.js
@@ -1,7 +1,70 @@
-import React from "react";
+import styles from "../styles/Home.module.css"
+import { Form, useNotification, Button } from "web3uikit"
+import { useMoralis, useWeb3Contract } from "react-moralis"
+import { ethers } from "ethers"
+import DBankAbi from "../constants/DBank.json"
+import networkMapping from "../constants/contractAddress.json"
 
-const Deposit = () => {
-  return <div>Deposit</div>;
-};
+export default function Home() {
+    const { chainId, account, isWeb3Enabled } = useMoralis()
+    const chainString = chainId ? parseInt(chainId).toString() : "31337"
 
-export default Deposit;
+    const dbankAddress = networkMapping[chainString]?.DBank[0]
+    console.log(dbankAddress)
+    const dispatch = useNotification()
+
+
+    const { runContractFunction } = useWeb3Contract()
+
+    async function DepositAmount(data) {
+        console.log("Depositing Amount")
+        const price = ethers.utils.parseUnits(data.data[0].inputResult, "ether").toString()
+
+
+
+        const DepositAmountOptions = {
+            abi: DBankAbi,
+            contractAddress: dbankAddress,
+            functionName: "deposit",
+            msgValue: price,
+
+          
+            
+        }
+
+        await runContractFunction({
+            params: DepositAmountOptions,
+            onSuccess: handleInitializeSuccess,
+            onError: (error) => console.log(error),
+        })
+    }
+
+    async function handleInitializeSuccess(tx) {
+        await tx.wait(1)
+        console.log("Amount deposited")
+        dispatch({
+            type: "success",
+            message: "Amount deposited",
+            title: "Amount deposited",
+            position: "topR",
+        })
+    }
+    return (
+        <div className={styles.container}>
+            <Form
+                onSubmit={DepositAmount}
+                data={[
+                   
+                    {
+                        name: "Amount (in ETH)",
+                        type: "number",
+                        value: "",
+                        key: "price",
+                    },
+                ]}
+                title="Deposit Money into your bank account"
+                id="Main Form"
+            />
+        </div>
+    )
+}
